22f329fc5e2f775b1db3ab5913da12402e79569f
arda <ardaolmez414@gmail.com>
2025-10-07 17:50:30 -0400

    Initial Commit

---
+++ Deen Buddy Advanced/Deen_Buddy_AdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deen_Buddy_AdvancedApp.swift
+//  Deen Buddy Advanced
+//
+//  Created by Arda Olmez on 2025-10-07.
+//
+
+import SwiftUI
+
+@main
+struct Deen_Buddy_AdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Deen Buddy AdvancedTests/Deen_Buddy_AdvancedTests.swift
@@ -0,0 +1,16 @@
+//
+//  Deen_Buddy_AdvancedTests.swift
+//  Deen Buddy AdvancedTests
+//
+//  Created by Arda Olmez on 2025-10-07.
+//
+
+import Testing
+
+struct Deen_Buddy_AdvancedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
