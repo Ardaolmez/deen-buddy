004962345884fed96032dc80ed5de6036b247d8c
Rana Shaheryar <ranasherry94@gmail.com>
2025-10-14 21:20:12 +0500

    Prayer Times Screen added

---
+++ Deen Buddy Advanced/Views/QuizView/QuizResultView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizResultView.swift
+//  Deen Buddy Advanced
+//
+//  Created by Rana Shaheryar on 10/14/25.
+//
+
+import SwiftUI
+
+struct QuizResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizResultView()
+}
