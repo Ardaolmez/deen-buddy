e14fecae9843408f6aa45dd224310fe6895105ec
arda <ardaolmez414@gmail.com>
2025-10-18 03:50:55 +0300

    widget extension

---
+++ Deen Buddy Widgets/Deen_Buddy_Widgets.swift
@@ -0,0 +1,88 @@
+//
+//  Deen_Buddy_Widgets.swift
+//  Deen Buddy Widgets
+//
+//  Created by Arda Olmez on 2025-10-18.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: AppIntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+    }
+
+    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: configuration)
+    }
+    
+    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        return Timeline(entries: entries, policy: .atEnd)
+    }
+
+//    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationAppIntent
+}
+
+struct Deen_Buddy_WidgetsEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Favorite Emoji:")
+            Text(entry.configuration.favoriteEmoji)
+        }
+    }
+}
+
+struct Deen_Buddy_Widgets: Widget {
+    let kind: String = "Deen_Buddy_Widgets"
+
+    var body: some WidgetConfiguration {
+        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+            Deen_Buddy_WidgetsEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
+        }
+    }
+}
+
+extension ConfigurationAppIntent {
+    fileprivate static var smiley: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "😀"
+        return intent
+    }
+    
+    fileprivate static var starEyes: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "🤩"
+        return intent
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Deen_Buddy_Widgets()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
---
+++ Deen Buddy Widgets/Deen_Buddy_WidgetsBundle.swift
@@ -0,0 +1,18 @@
+//
+//  Deen_Buddy_WidgetsBundle.swift
+//  Deen Buddy Widgets
+//
+//  Created by Arda Olmez on 2025-10-18.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Deen_Buddy_WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Deen_Buddy_Widgets()
+        Deen_Buddy_WidgetsControl()
+        Deen_Buddy_WidgetsLiveActivity()
+    }
+}
